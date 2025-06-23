ae4e08e9bee27975e8ea8507627d5a1f009d78c1
Guðrún Ólafsdóttir <Gudrun.Olafsdottir@landsbankinn.is>
2025-06-23 17:09:21 +0000

    sync pico to git helper

---
+++ sync.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PICO_DIR="/Volumes/CIRCUITPY"
+
+
+if [ ! -d $PICO_DIR ];then
+	echo "PICO not connected"
+	echo "exiting"
+	exit
+fi
+
+
+
+function to_board () {
+
+	cd $SCRIPT_DIR
+	cp ./*py $PICO_DIR
+	cp -a ./tmc/ ${PICO_DIR}/lib
+
+}
+
+function to_git () {
+
+	cd $PICO_DIR
+	cp ./*py $SCRIPT_DIR
+	cp ./lib/tmc $SCRIPT_DIR
+
+
+}
+
+if [[ "$1" == "to_board" ]];then
+	to_board
+elif [[ "$1" == "to_git" ]];then
+	to_git
+else
+	echo "usage is $0 <to_board|to_git>"
+fi
+
